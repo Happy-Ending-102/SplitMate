c15bb57ebb669c57266cbc0353f6ef3d1e2f74c0
Sude Kaplan <kaplanzeynep20@gmail.com>
2025-05-07 23:04:49 +0300

    addGroup fxml and controller (I forgot saving :))

--- src/main/java/com/splitmate/controller/AddGroupController.java
+++ src/main/java/com/splitmate/controller/AddGroupController.java
@@ -12,7 +12,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class addGroupController implements Initializable{
+public class AddGroupController implements Initializable{
 
     @FXML
     private Button addImageButton;
