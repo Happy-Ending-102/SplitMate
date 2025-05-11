6e43951e899ef92c55bc402eed39b0a4a215b78a
Darkeenji <19bilge13@gmail.com>
2025-05-11 15:18:06 +0300

    fix incorrect parameter

--- src/main/java/com/splitmate/service/PaymentCalculator.java
+++ src/main/java/com/splitmate/service/PaymentCalculator.java
@@ -3,9 +3,9 @@ package com.splitmate.service;
 
 import java.util.List;
 import com.splitmate.model.Payment;
-import com.splitmate.model.Group;
+import com.splitmate.model.User;
 import com.splitmate.model.Friendship;
 
 public interface PaymentCalculator {
-    List<Payment> calculate(Group group, List<Friendship> friendships);
+    List<Payment> calculate(List<User> users, List<Friendship> friendships);
 }
